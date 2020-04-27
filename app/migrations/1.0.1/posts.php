b6526da35519e20fcf418060214d2bde740dfe60
Golovach Mykola <777mykola777@gmail.com>
2020-04-27 17:31:18 +0300

    dggd

---
+++ app/migrations/1.0.1/posts.php
@@ -0,0 +1,94 @@
+<?php
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Migrations\Mvc\Model\Migration;
+
+/**
+ * Class PostsMigration_101
+ */
+class PostsMigration_101 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('posts', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 10,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'user_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'unsigned' => true,
+                            'notNull' => true,
+                            'size' => 10,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'title',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'user_id'
+                        ]
+                    ),
+                    new Column(
+                        'content',
+                        [
+                            'type' => Column::TYPE_LONGTEXT,
+                            'notNull' => true,
+                            'after' => 'title'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY')
+                ],
+                'options' => [
+                    'table_type' => 'BASE TABLE',
+                    'auto_increment' => '4',
+                    'engine' => 'InnoDB',
+                    'table_collation' => 'utf8mb4_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
---
+++ app/migrations/1.0.1/users.php
@@ -0,0 +1,112 @@
+<?php
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Migrations\Mvc\Model\Migration;
+
+/**
+ * Class UsersMigration_101
+ */
+class UsersMigration_101 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('users', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 10,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'email',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'first_name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'email'
+                        ]
+                    ),
+                    new Column(
+                        'last_name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'first_name'
+                        ]
+                    ),
+                    new Column(
+                        'role',
+                        [
+                            'type' => Column::TYPE_ENUM,
+                            'notNull' => true,
+                            'size' => "'admin','manager'",
+                            'after' => 'last_name'
+                        ]
+                    ),
+                    new Column(
+                        'password',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 255,
+                            'after' => 'role'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('email', ['email'], 'UNIQUE')
+                ],
+                'options' => [
+                    'table_type' => 'BASE TABLE',
+                    'auto_increment' => '3',
+                    'engine' => 'InnoDB',
+                    'table_collation' => 'utf8mb4_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
